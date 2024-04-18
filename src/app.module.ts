1b656e41cfb6616e56cb84b7b583149752d00c54
Mateus Conceição <113058396+conceicaomateus@users.noreply.github.com>
2024-04-18 22:45:01 +0000

    feat: add address crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PersonModule } from './person/person.module';
 import { StudentModule } from './student/student.module';
 import { UserModule } from './user/user.module';
 import { AccessProfileModule } from './access-profile/access-profile.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [ProjectsModule, PersonModule, StudentModule, UserModule, AccessProfileModule],
+  imports: [ProjectsModule, PersonModule, StudentModule, UserModule, AccessProfileModule, AddressModule],
   controllers: [],
   providers: [],
 })
