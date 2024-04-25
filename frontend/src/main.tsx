6057483a4e5f1dabd59822e2ec66bce0a419ab5d
Mateus Conceição <113058396+conceicaomateus@users.noreply.github.com>
2024-04-25 22:58:04 +0000

    config shadcn ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./main.css"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../components/ui/accordion"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Projeto Abex</h1>
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+
   </React.StrictMode>,
 )
