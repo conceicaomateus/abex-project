69319991b7fa26ed2be46cbfdccac0d635ab0345
Mateus Conceição <mateusm@optidata.cloud>
2024-04-26 11:39:33 -0300

    feat: config prettier + eslint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "./main.css"
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "../components/ui/accordion"
+import './global.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-    </AccordionItem>
-  </Accordion>
-
+    <App />
   </React.StrictMode>,
-)
+);
