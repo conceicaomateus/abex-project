4c999fb4f6ebefd8a0d6e2bd65e7ec6ba21b5007
Mateus Conceição <mateusm@optidata.cloud>
2024-04-26 11:38:37 -0300

    feat: config prettier + eslint

--- backend/src/access-profile/dto/update-access-profile.dto.ts
+++ backend/src/access-profile/dto/update-access-profile.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAccessProfileDto } from './create-access-profile.dto';
 
-export class UpdateAccessProfileDto extends PartialType(CreateAccessProfileDto) {}
+export class UpdateAccessProfileDto extends PartialType(
+  CreateAccessProfileDto,
+) {}
